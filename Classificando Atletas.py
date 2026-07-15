e26cfa598af40574a12e6894e7feca992cbd5714
DoraBellaAlice <doraliceribeirodasilva64@gmail.com>
2026-07-15 08:46:02 -0300

    Add files via upload

---
+++ Classificando Atletas.py
@@ -0,0 +1,17 @@
+#  Classificando Atletas
+
+from  datetime import date
+atual =  date.today().year
+nascimento = int(input('Ano de Nascimento '))
+idade = atual - nascimento
+print('O Atleta tem {} anos'.format(idade))
+if  idade <= 9:
+    print('Classificação: Mirim')
+elif idade <= 14:
+    print('Classificação: Infantil')
+elif idade <= 19:
+    print('Classificação: Junior')
+elif idade <= 25:
+    print('Classificação: Sênior')
+else:
+    print('Classificação Master')
