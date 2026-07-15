34104b3c9becfe67eb7a1d1b8baa3c5e766e52d3
DoraBellaAlice <doraliceribeirodasilva64@gmail.com>
2026-07-15 09:14:37 -0300

    Add files via upload

---
+++ Financiamento-casa.py
@@ -0,0 +1,16 @@
+# Aprovação-financiamento-casa
+
+from sys import float_info
+
+casa = float(input('Valor da casa R$ ' ))
+salário = float(input('Salário do comprador: R$ '))
+anos = int(input('Quantos anos de financiamento? '))
+prestação = casa / (anos * 12)
+minimo = salário * 30 / 100
+print('Para pagar uma casa de {:.2f} em {} anos '.format(casa , anos), end='') # quebra de linha
+print('a prestação será de R$ {:.2f}'.format(prestação))
+if prestação <= minimo:
+    print('Emprestimo pode ser concedido!')
+else:
+    print('Emprestimo negado!')
+
